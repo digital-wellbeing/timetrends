36810df976766abf2ab5af90d65beaf63e432dee
Matti Vuorre <vuorre@gmail.com>
2020-04-27 09:30:12 +0100

    Use renv (#4)

--- R/packages.R
+++ R/packages.R
@@ -1,34 +1,12 @@
 # Attach packages required in project
-# This script also facilitates running on a cluster by
-# installing required packages if not already installed
-options(repos = "http://cran.rstudio.com/")
-packages <- c(
-  "drake",
-  "clustermq",
-  "car",
-  "rmarkdown",
-  "lme4",
-  "brms",
-  "flexdashboard",
-  "lubridate",
-  "ggstance",
-  "plotly",
-  "scales",
-  "janitor",
-  "broom",
-  "knitr",
-  "glue",
-  "tidybayes",
-  "multcomp",
-  "emmeans",
-  "patchwork",
-  "labelled",
-  "haven",
-  "tidyverse"
-  )
-packages_new <- packages[!(packages %in% installed.packages()[,"Package"])]
-if(length(packages_new)) install.packages(packages_new)
-
-# Load packages
-invisible(lapply(packages, library, character.only = TRUE))
-rm(packages, packages_new)
+# options(repos = "http://cran.rstudio.com/")  # If needed for cluster
+library(drake)
+library(plotly)
+library(scales)
+library(broom)
+library(knitr)
+library(glue)
+library(emmeans)
+library(labelled)
+library(haven)
+library(tidyverse)
