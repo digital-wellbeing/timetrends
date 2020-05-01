67d4fbab9836b964061fb1d04e7cd0a714a36a23
Matti Vuorre <vuorre@gmail.com>
2020-05-01 09:18:52 +0100

    Include fst package

--- R/packages.R
+++ R/packages.R
@@ -1,6 +1,7 @@
 # Attach packages required in project
 # options(repos = "http://cran.rstudio.com/")  # If needed for cluster
 library(drake)
+library(fst)
 library(plotly)
 library(scales)
 library(broom)
